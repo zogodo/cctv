e628fbc21a99570200a696e221708e7fd56096cc
zogodo <742782908@qq.com>
2024-04-16 15:30:55 +0800

    copy from android_demo

---
+++ app/src/main/java/me/zogodo/demo/JsInterface.java
@@ -0,0 +1,23 @@
+package me.zogodo.demo;
+
+import android.webkit.JavascriptInterface;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class JsInterface
+{
+    @JavascriptInterface
+    public static String GetString() {
+ 
+        Map<String, Integer> map = new HashMap<>();
+        map.put("aa", 10);
+        map.put("bb", 23);
+
+        JSONObject json = new JSONObject(map);
+        return  json.toString();
+    }
+
+}
---
+++ app/src/main/java/me/zogodo/demo/MainActivity.java
@@ -0,0 +1,47 @@
+package me.zogodo.demo;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity
+{
+    public static MainActivity me;
+    public static String indexUrl = "file:///android_asset/web/index.html";
+    public static WebView webView = null;
+    long exitTime = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        MainActivity.me = this;
+
+        webView = new MyWebView(this);
+        webView.loadUrl(indexUrl);
+        this.setContentView(webView);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            // 判断是否可后退，是则后退，否则退出程序
+            if (((System.currentTimeMillis() - exitTime) > 3000)) {
+                Toast.makeText(getApplicationContext(), "再按一次返回 退出程序", Toast.LENGTH_SHORT).show();
+                exitTime = System.currentTimeMillis();
+            } else {
+                finish();
+                System.exit(0);
+            }
+        }
+    }
+}
