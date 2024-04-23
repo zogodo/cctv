68c8b766960496d1094f0265ab9749c77650f313
zogodo <742782908@qq.com>
2024-04-23 11:06:19 +0800

    up

--- app/src/main/java/me/zogodo/cctv/MainActivity.java
+++ app/src/main/java/me/zogodo/cctv/MainActivity.java
@@ -1,97 +1,10 @@
 package me.zogodo.cctv;
 
-//import android.os.Bundle;
-//import android.view.KeyEvent;
-//
-//import org.mozilla.geckoview.GeckoRuntime;
-//import org.mozilla.geckoview.GeckoRuntimeSettings;
-//import org.mozilla.geckoview.GeckoSession;
-//import org.mozilla.geckoview.GeckoSessionSettings;
-//import org.mozilla.geckoview.GeckoView;
-//import android.widget.Toast;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-
-//class MainActivity extends AppCompatActivity
-//{
-//
-//    GeckoView view_gecko = null;
-//    GeckoSession session = null;
-//    static GeckoRuntime runtime = null;
-//
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        getSupportActionBar().hide();
-//        MainActivity.me = this;
-//
-//        //webView = new MyWebView(this);
-//        //webView.loadUrl(cctv_urls[channel]);
-//        ////webView.loadUrl("https://cn.bing.com/?ensearch=1&FORM=BEHPTB");
-//        //this.setContentView(webView);
-//
-//        view_gecko = new GeckoView(this);
-//
-//        GeckoRuntimeSettings.Builder builder = new GeckoRuntimeSettings.Builder()
-//                .javaScriptEnabled(true)
-//                .doubleTapZoomingEnabled(false)
-//                .inputAutoZoomEnabled(false)
-//                .consoleOutput(true);
-//        session = new GeckoSession();
-//        runtime = GeckoRuntime.create(this,builder.build());
-//        session.open(runtime);
-//        view_gecko.setSession(session);
-//        session.getSettings().setAllowJavascript(true);
-//        session.getPanZoomController().setIsLongpressEnabled(false);
-//        session.loadUri("https://tv.cctv.com/live/cctv13/m/");
-//
-//        setContentView(view_gecko);
-//    }
-//
-//    @Override
-//    public boolean onKeyDown(int keyCode, KeyEvent event) {
-//        Toast.makeText(getApplicationContext(), "keyCode:" + keyCode, Toast.LENGTH_SHORT).show();
-//
-//        switch (keyCode) {
-//            case KeyEvent.KEYCODE_0:
-//                channel = c_cctv13;
-//                break;
-//            case KeyEvent.KEYCODE_1:
-//            case KeyEvent.KEYCODE_2:
-//            case KeyEvent.KEYCODE_3:
-//            case KeyEvent.KEYCODE_4:
-//            case KeyEvent.KEYCODE_5:
-//            case KeyEvent.KEYCODE_6:
-//            case KeyEvent.KEYCODE_7:
-//            case KeyEvent.KEYCODE_8:
-//            case KeyEvent.KEYCODE_9:
-//                channel = keyCode - KeyEvent.KEYCODE_1;
-//                break;
-//            case KeyEvent.KEYCODE_DPAD_UP:
-//            case KeyEvent.KEYCODE_VOLUME_UP:
-//            case KeyEvent.KEYCODE_MENU:
-//                channel = (channel+1) % cctv_urls.length;
-//                break;
-//            case KeyEvent.KEYCODE_DPAD_DOWN:
-//            case KeyEvent.KEYCODE_VOLUME_DOWN:
-//                channel = (channel-1) % cctv_urls.length;
-//                break;
-//            default:
-//                return super.onKeyDown(keyCode, event);
-//        }
-//        session.loadUri(cctv_urls[channel]); //换台
-//        return true;
-//    }
-//
-
-//
-//}
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.widget.Toast;
 
 import org.mozilla.geckoview.GeckoRuntime;
@@ -150,18 +63,48 @@ public class MainActivity extends AppCompatActivity
         view_gecko.setSession(session);
         session.getSettings().setAllowJavascript(true);
         session.getPanZoomController().setIsLongpressEnabled(false);
-        session.loadUri("https://tv.cctv.com/live/cctv13/m/");
+        session.loadUri(cctv_urls[channel]);
 
         setContentView(view_gecko);
     }
 
     @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        Toast.makeText(getApplicationContext(), "keyCode:" + keyCode, Toast.LENGTH_SHORT).show();
+
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_0:
+                channel = c_cctv13;
+                break;
+            case KeyEvent.KEYCODE_1:
+            case KeyEvent.KEYCODE_2:
+            case KeyEvent.KEYCODE_3:
+            case KeyEvent.KEYCODE_4:
+            case KeyEvent.KEYCODE_5:
+            case KeyEvent.KEYCODE_6:
+            case KeyEvent.KEYCODE_7:
+            case KeyEvent.KEYCODE_8:
+            case KeyEvent.KEYCODE_9:
+                channel = keyCode - KeyEvent.KEYCODE_1;
+                break;
+            case KeyEvent.KEYCODE_DPAD_UP:
+            case KeyEvent.KEYCODE_VOLUME_UP:
+            case KeyEvent.KEYCODE_MENU:
+                channel = (channel+1) % cctv_urls.length;
+                break;
+            case KeyEvent.KEYCODE_DPAD_DOWN:
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                channel = (channel-1) % cctv_urls.length;
+                break;
+            default:
+                return super.onKeyDown(keyCode, event);
+        }
+        session.loadUri(cctv_urls[channel]); //换台
+        return true;
+    }
+
+    @Override
     public void onBackPressed() {
-        //if (webView != null && webView.canGoBack()) {
-        //    webView.goBack();
-        //    return;
-        //}
-        // 判断是否可后退，是则后退，否则退出程序
         if (((System.currentTimeMillis() - exitTime) > 3000)) {
             Toast.makeText(getApplicationContext(), "再按一次返回 退出程序", Toast.LENGTH_SHORT).show();
             exitTime = System.currentTimeMillis();
